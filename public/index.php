e685ed7c9b0cce1e12b7ef086280c3e286802951
Dirk Engels <d.engels@dirkengels.com>
2011-02-20 21:47:52 +0100

    updated index with namespaces class loader

--- public/index.php
+++ public/index.php
@@ -30,10 +30,14 @@ set_include_path(
 // Custom Autoloader
 function autoload($className) {
 	foreach($GLOBALS['includePaths'] as $path) {
-    	if (file_exists($path . '/' . str_replace('_','/',$className) . '.php')) {
-    		include_once($path . '/' . str_replace('_','/',$className) . '.php');
-    	}
-    }
+		$classNamespaced = $path .'/' . str_replace('\\', '/', $className) . '.php';
+		$classConvention = $path . '/' . str_replace('_','/',$className) . '.php';
+		if (file_exists($classNamespaced)) {
+			include_once ($classNamespaced);
+		} elseif (file_exists($classConvention)) {
+			include_once($classConvention);
+		}
+	}
 }
 spl_autoload_register('autoload');
 
