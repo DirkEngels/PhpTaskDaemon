3df42a9a31031fe94d96de2874b540c42f74f820
Dirk Engels <d.engels@dirkengels.com>
2011-03-05 03:03:20 +0100

    initial queue & executor interfaces

---
+++ lib/PhpTaskDaemon/Task/Executor/AbstractClass.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package PhpTaskDaemon
+ * @subpackage Task\Executor
+ * @copyright Copyright (C) 2010 Dirk Engels Websolutions. All rights reserved.
+ * @author Dirk Engels <d.engels@dirkengels.com>
+ * @license https://github.com/DirkEngels/PhpTaskDaemon/blob/master/doc/LICENSE
+ */
+namespace PhpTaskDaemon\Task\Executor;
+
+class AbstractClass {
+
+}
---
+++ lib/PhpTaskDaemon/Task/Queue/AbstractClass.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package PhpTaskDaemon
+ * @subpackage Task\Queue
+ * @copyright Copyright (C) 2010 Dirk Engels Websolutions. All rights reserved.
+ * @author Dirk Engels <d.engels@dirkengels.com>
+ * @license https://github.com/DirkEngels/PhpTaskDaemon/blob/master/doc/LICENSE
+ */
+namespace PhpTaskDaemon\Task\Queue;
+
+class AbstractClass {
+
+}
