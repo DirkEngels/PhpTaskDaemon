c7a2b4f5def20235242be72c5d5ec777814eb927
Dirk Engels <d.engels@dirkengels.com>
2011-03-05 03:49:17 +0100

    moved main classes to subdirectory core

--- lib/PhpTaskDaemon/Core/Command.php
+++ lib/PhpTaskDaemon/Core/Command.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @package Dew
- * @subpackage Daemon
+ * @package PhpTaskDaemon
+ * @subpackage Core
  * @copyright Copyright (C) 2010 Dirk Engels Websolutions. All rights reserved.
  * @author Dirk Engels <d.engels@dirkengels.com>
  * @license https://github.com/DirkEngels/PhpTaskDaemon/blob/master/doc/LICENSE
