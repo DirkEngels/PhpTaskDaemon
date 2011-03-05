9f404c8e37340f6787344717076307fbfad4a104
Dirk Engels <d.engels@dirkengels.com>
2011-03-05 03:50:26 +0100

    added concept classes for status of a task

---
+++ lib/PhpTaskDaemon/Task/Status/File.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @package PhpTaskDaemon
+ * @subpackage Task\Status
+ * @copyright Copyright (C) 2010 Dirk Engels Websolutions. All rights reserved.
+ * @author Dirk Engels <d.engels@dirkengels.com>
+ * @license https://github.com/DirkEngels/PhpTaskDaemon/blob/master/doc/LICENSE
+ */
+namespace PhpTaskDaemon\Task\Status;
+
+class File implements InterfaceClass {
+	
+	public function run() {
+		
+	}
+}
---
+++ lib/PhpTaskDaemon/Task/Status/SharedMemory.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @package PhpTaskDaemon
+ * @subpackage Task\Status
+ * @copyright Copyright (C) 2010 Dirk Engels Websolutions. All rights reserved.
+ * @author Dirk Engels <d.engels@dirkengels.com>
+ * @license https://github.com/DirkEngels/PhpTaskDaemon/blob/master/doc/LICENSE
+ */
+namespace PhpTaskDaemon\Task\Status;
+
+class SharedMemory implements InterfaceClass {
+	
+	public function run() {
+		
+	}
+
+}
