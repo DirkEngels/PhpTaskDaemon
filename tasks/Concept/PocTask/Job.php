2782fd2b46c7729e220f4d0f5dece1452acc4ee9
Dirk Engels <d.engels@dirkengels.com>
2011-03-19 15:19:49 +0100

    added loader and example

---
+++ tasks/Concept/PocTask/Job.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace PocTask;
+
+class Job extends \PhpTaskDaemon\Job\AbstractClass implements \PhpTaskDaemon\Job\InterfaceClass {
+
+	protected $_inputFields = array('sleepTime');
+	
+	/**
+	 * 
+	 * Check the input array for the needed keys; in this case only a sleepTime
+	 * variable is expected
+	 * @return bool;`
+	 */
+	public function checkInput() {
+		return array_key_exists('sleepTime', $this->_input);
+	}
+}
+
---
+++ tasks/Concept/PocTask/Queue.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace PocTask;
+use \PhpTaskDaemon\Queue as Queue;
+
+class Queue extends Queue\AbstractClass implements Queue\InterfaceClass {
+
+	/**
+	 * Fills a queue with a random number of tasks (0 - 30). The input for each
+	 * task will be a single variable containing the amount of miliseconds to
+	 * sleep.
+	 * @see PhpTaskDaemon\Queue.InterfaceClass::load()
+	 */
+	public function load() {
+		$queue = array();
+		if (count($queue)==0) {
+			for ($i=0; $i<rand(0,30); $i++) {
+				$job = new Job(
+					'jobId: ' . $i, 
+					array('sleepTime' => rand(100000, 500000))
+				);
+				array_push($queue, $job);
+			}
+		}
+		return $queue;
+	}
+	
+}
+
