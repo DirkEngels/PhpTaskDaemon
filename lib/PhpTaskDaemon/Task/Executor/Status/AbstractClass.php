3e912b16eccb6366c25a595b25bff8163e1a8800
Dirk Engels <d.engels@dirkengels.com>
2011-04-05 23:43:51 +0200

    fix shm identifier

--- lib/PhpTaskDaemon/Task/Executor/Status/AbstractClass.php
+++ lib/PhpTaskDaemon/Task/Executor/Status/AbstractClass.php
@@ -24,28 +24,46 @@ abstract class AbstractClass {
 		unset($this->_sharedMemory); 
 	}
 		
+	/**
+	 *
+	 * Returns the shared memory object
+	 * @return PhpTaskDaemon\SharedMemory
+	 */
 	public function getSharedMemory() {
 		return $this->_sharedMemory;
 	}
+
+	/**
+	 *
+	 * Sets a shared memory object
+	 * @param \PhpTaskDaemon\Daemon\Ipc\SharedMemory $sharedMemory
+	 * @return $this
+	 */
 	public function setSharedMemory($sharedMemory) {
 		if (!is_a($sharedMemory, '\PhpTaskDaemon\Daemon\Ipc\SharedMemory')) {
-			$sharedMemory = new \PhpTaskDaemon\Daemon\Ipc\SharedMemory('test');
+			$sharedMemory = new \PhpTaskDaemon\Daemon\Ipc\SharedMemory('status-' . getmypid());
+			$this->_sharedMemory = $sharedMemory;
 		}
-		$this->_sharedMemory = $sharedMemory;
+		return $this;
 	}
+	
 
 	public function get($key = null) {
+    	if (!is_a($this->_sharedMemory, '\PhpTaskDaemon\Daemon\Ipc\SharedMemory')) {
+    		return false;
+    	}
 		if ($key != null) {
 			return $this->_sharedMemory->getVar($key);
 		}
 		return $this->_sharedMemory->getKeys();
 	}
     public function set($percentage, $message = null) {
-    	if (is_a($this->_sharedMemory, '\PhpTaskDaemon\Daemon\Ipc\SharedMemory')) {
-	    	$this->_sharedMemory->setVar('percentage', $percentage);
-	    	if ($message != null) {
-	    		$this->_sharedMemory->setVar('message', $message);
-	    	}
+    	if (!is_a($this->_sharedMemory, '\PhpTaskDaemon\Daemon\Ipc\SharedMemory')) {
+    		return false;
     	}
+    	$this->_sharedMemory->setVar('percentage', $percentage);
+    	if ($message != null) {
+    		$this->_sharedMemory->setVar('message', $message);
+    	}	
     }
 }
