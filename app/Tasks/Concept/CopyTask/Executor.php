6abeb60e1656c2fb2eaf1e86f45e3fa2a6b96147
Dirk Engels <d.engels@dirkengels.com>
2011-04-12 02:08:45 +0200

    added comment

--- app/Tasks/Concept/CopyTask/Executor.php
+++ app/Tasks/Concept/CopyTask/Executor.php
@@ -15,19 +15,17 @@ class Executor extends PTDTE\AbstractClass implements PTDTE\InterfaceClass {
 
 		// Sleep
 		$sleepTimeProgress = round($job->getInputVar('sleepTime')/10);
-		$this->updateStatus(0, 'Initializing task');
 		for ($i=1; $i<10; $i++) {
 			usleep($sleepTimeProgress);
-			echo ".";
 			$this->updateStatus(($i*10), 'Task data: ' . $job->getJobId());
 		}
-		echo "\n";
-		$this->updateStatus(100, 'Task finished');
 
-		// Output
+		// Return Status
 		$returnStatus = (rand(0,1)==1) 
 			? PTDTQS\BaseClass::STATUS_DONE 
-			: PTDTQS\BaseClass::STATUS_FAILED; 
+			: PTDTQS\BaseClass::STATUS_FAILED;
+
+		// Output
 		$job->setOutput(array(
 			'returnStatus' => $returnStatus,
 			'waitTime' => rand(1,5)
