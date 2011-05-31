de47a164882dc9014c29b6bf8cdd5a5a069232e6
Dirk Engels <d.engels@dirkengels.com>
2011-05-31 23:37:14 +0200

    Initial processes

---
+++ lib/PhpTaskDaemon/Task/Manager/Process/Parallel.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package PhpTaskDaemon
+ * @subpackage Task\Manager\Process
+ * @copyright Copyright (C) 2011 Dirk Engels Websolutions. All rights reserved.
+ * @author Dirk Engels <d.engels@dirkengels.com>
+ * @license https://github.com/DirkEngels/PhpTaskDaemon/blob/master/doc/LICENSE
+ */
+
+namespace PhpTaskDaemon\Task\Manager\Process;
+
+class Parallel extends AbstractClass implements InterfaceClass {
+    
+}
