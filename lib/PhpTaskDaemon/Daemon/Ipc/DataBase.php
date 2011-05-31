b32882b3c4718a93bde619d247d70d00b4529295
Dirk Engels <d.engels@dirkengels.com>
2011-06-01 00:44:03 +0200

    added None & Database Daemon IPC Adapters

---
+++ lib/PhpTaskDaemon/Daemon/Ipc/DataBase.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * @package PhpTaskDaemon
+ * @subpackage Daemon\Ipc
+ * @copyright Copyright (C) 2011 Dirk Engels Websolutions. All rights reserved.
+ * @author Dirk Engels <d.engels@dirkengels.com>
+ * @license https://github.com/DirkEngels/PhpTaskDaemon/blob/master/doc/LICENSE
+ */
+
+namespace PhpTaskDaemon\Daemon\Ipc;
+
+/**
+ * 
+ * The Daemon\Ipc\DataBase class is responsible for storing and retrieving
+ * inter process communication data from the database.
+ */
+class DataBase extends AbstractClass implements InterfaceClass {
+    
+    /**
+     * 
+     * Returns an empty array
+     * @return array
+     */
+    public function getKeys() {
+        return array();
+    }
+    
+    /**
+     * 
+     * Returns nothing (null) 
+     * @param string $key
+     * @return null
+     */
+    public function getVar($key) {
+        return null;
+    }
+
+    /**
+     * 
+     * Sets nothing
+     * @param string $key
+     * @param mixed $value
+     * @return bool
+     */
+    public function setVar($key, $value) {
+        return true;
+    }
+
+    /**
+     * 
+     * Increments nothing
+     * @param string $key
+     * @return bool
+     */
+    public function incrementVar($key) {
+        return true;
+    }
+
+    /**
+     * 
+     * Decrements nothing
+     * @param string $key
+     * @return bool
+     */
+    public function decrementVar($key) {
+        return true;
+    }
+
+    /**
+     * 
+     * Removes nothing 
+     * @param string $key
+     * @return bool
+     */
+    public function removeVar($key) {
+        return true;
+    }
+    
+    /**
+     * 
+     * Removes nothing
+     * @return bool
+     */
+    public function remove() {
+        return true;
+    }
+}
