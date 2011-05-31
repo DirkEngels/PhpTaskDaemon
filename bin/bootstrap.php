d5817e49c6b61d832dee362d8529dc6a36a8840f
Dirk Engels <d.engels@dirkengels.com>
2011-05-31 22:31:34 +0200

    made the autoloader method name unique

--- bin/bootstrap.php
+++ bin/bootstrap.php
@@ -3,7 +3,7 @@
 // Set include paths
 define('PROJECT_ROOT', realpath(__DIR__ .'/../'));
 define('APPLICATION_PATH', realpath(\PROJECT_ROOT .'/app'));
-define('TASKDIR_PATH', realpath(\PROJECT_ROOT .'/tasks'));
+define('TASKDIR_PATH', realpath(\PROJECT_ROOT .'/app/Tasks'));
 define('LIBRARY_PATH', realpath(\PROJECT_ROOT .'/lib'));
 define('TMP_PATH', realpath(\PROJECT_ROOT .'/tmp'));
 define('APPLICATION_ENV', 'development');
@@ -23,15 +23,18 @@ set_include_path(
 );
 
 // Custom Autoloader
-function autoload($className) {
+function __autoloadPhpTaskDaemon($className) {
 	foreach($GLOBALS['includePaths'] as $path) {
 		$classNamespaced = $path .'/' . str_replace('\\', '/', $className) . '.php';
 		$classConvention = $path . '/' . str_replace('_','/',$className) . '.php';
+		$classParent = $path . '/' . substr($className, 0, strrpos($className, '/')) . '.php';
 		if (file_exists($classNamespaced)) {
 			include_once ($classNamespaced);
 		} elseif (file_exists($classConvention)) {
 			include_once($classConvention);
+		} elseif (file_exists($classParent)) {
+			include_once($classParent);
 		}
 	}
 }
-spl_autoload_register('autoload');
+spl_autoload_register('__autoloadPhpTaskDaemon');
