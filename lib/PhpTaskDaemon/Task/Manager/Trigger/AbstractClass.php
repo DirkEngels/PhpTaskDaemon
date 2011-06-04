dcb880680be3cf835be4db1ae7f842846aee7c6b
Dirk Engels <d.engels@dirkengels.com>
2011-06-04 05:08:45 +0200

    added getter & setter for the queue component

--- lib/PhpTaskDaemon/Task/Manager/Trigger/AbstractClass.php
+++ lib/PhpTaskDaemon/Task/Manager/Trigger/AbstractClass.php
@@ -10,7 +10,32 @@
 namespace PhpTaskDaemon\Task\Manager\Trigger;
 
 abstract class AbstractClass {
+	protected $_queue;
     
+    /**
+     * 
+     * Returns the current loaded queue array
+     * @return \PhpTaskDaemon\Task\Queue\AbstractClass
+     */
+    public function getQueue() {
+        return $this->_queue;
+    }
+
+    /**
+     * 
+     * Sets the current queue to process.
+     * @param \PhpTaskDaemon\Task\Queue\AbstractClass $queue
+     * @return $this
+     */
+    public function setQueue($queue) {
+        if (!is_a($queue, '\PhpTaskDaemon\Task\Queue\AbstractClass')) {
+            $queue = new \PhpTaskDaemon\Task\Queue\BaseClass();
+        }
+        $this->_queue = $queue;
+
+        return $this;
+    }
+
     public function timeToWait() {
     	return 1;
     }
