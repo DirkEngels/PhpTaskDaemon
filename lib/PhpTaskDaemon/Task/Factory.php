b043cab04d5ce1867b86592e45437b64a6a649e4
Dirk Engels <d.engels@dirkengels.com>
2011-06-07 22:12:47 +0200

    refactored the task factory

--- lib/PhpTaskDaemon/Task/Factory.php
+++ lib/PhpTaskDaemon/Task/Factory.php
@@ -1,91 +1,102 @@
 <?php
+/**
+ * @package PhpTaskDaemon
+ * @subpackage Task
+ * @copyright Copyright (C) 2011 Dirk Engels Websolutions. All rights reserved.
+ * @author Dirk Engels <d.engels@dirkengels.com>
+ * @license https://github.com/DirkEngels/PhpTaskDaemon/blob/master/doc/LICENSE
+ */
 
 namespace PhpTaskDaemon\Task;
-
 use \PhpTaskDaemon\Task\Exception as Exception;
 
+/**
+ * The Task Factory object uses the Factory Design Pattern for instantiating
+ * task related objects. It handles different kind of components and is also
+ * able to create a complete task component instantiation. (Which is basically
+ * a manager object with manager trigger and manager process instances, a queue
+ * and queue statistics instances and also an executor and executor status
+ * instances, which are injected into the manager base class.  
+ *
+ */
 class Factory {
+	const TYPE_MANAGER = 'manager';
+	
+    const TYPE_TRIGGER = 'trigger';
+    const TYPE_QUEUE = 'queue';
+    const TYPE_STATISTICS = 'statistics';
+
 	const TYPE_PROCESS = 'process';
-	const TYPE_TRIGGER = 'trigger';
+    const TYPE_EXECUTOR = 'executor';
     const TYPE_STATUS = 'status';
-	const TYPE_QUEUE = 'queue';
-	const TYPE_STATISTICS = 'statistics';
-	
-	
-	/**
-	 * Returns the classname based on the taskName and objectType
-	 * @param string $taskName
-	 * @param string $objectType
-	 * @return string
-	 */
-	protected function _getClassName($taskName, $objectType) {
-		return '\\Tasks\\' . $taskName . '\\' . ucfirst($objectType);
+
+
+	public static function get($taskName) {
+		return self::get2($taskName);
 	}
-	
-	/**
-	 * Checks if a objectType class of a specific manager exists
-	 * @param string $taskName
-	 * @param string $objectType
-	 * @return null|stdClass
-	 */
-	protected function _getObjectClass($taskName, $objectType) {
-		$className = $this->_getClassName($taskName, $objectType);
-		if (class_exists($className)) {
-			return new $className();
-		}
-		return false;
-	}
-	
-	/**
-	 * Checks if task specific configuration options for the objectType are
-	 * set.
-	 * @param string $taskName
-	 * @param string $objectType
-	 * @return null|stdClass
-	 */
-	protected function _getObjectTaskConfig($taskName, $objectType) {
-	   return false;
-	}
-	
-	/**
-	 * Checks if default configuration options for the objectType are set.
-     * @param string $taskName
-     * @param string $objectType
-     * @return null|stdClass
-     */
-	protected function _getObjectDefaultConfig($taskName, $objectType) {
-	}
-	
-	/**
-	 * Returns the hardcoded default object for a specific type.
-	 * @param string $objectType
-	 * @return null|StdClass
-	 */
-	protected function _getObjectHardCoded($objectType) {
-		switch($objectType) {
-			case 'process':
-				return new \PhpTaskDaemon\Task\Manager\Process\BaseClass();
-            case 'trigger':
-                return new \PhpTaskDaemon\Task\Manager\Trigger\BaseClass();
-            case 'status':
-                return new \PhpTaskDaemon\Task\Executor\Status\BaseClass();
-            case 'queue':
-                return new \PhpTaskDaemon\Task\Queue\BaseClass();
-            case 'statistics':
-                return new \PhpTaskDaemon\Task\Queue\Statistics\BaseClass();
-		}
-		throw new Exception\UndefinedObjectType('Unknown object type: ' . $objectType);
+
+
+	public static function get1($taskName) {
+		// Instantiate Executor
+		$executor = self::getComponentType($taskName, self::TYPE_EXECUTOR);
+		$executor->setStatus(
+            self::getComponentType($taskName, self::TYPE_STATUS)
+		);
 		
-		return false;
+		// Instantiate Queue
+		$queue = self::getComponentType($taskName, self::TYPE_QUEUE);
+		$queue->setStatistics(
+            self::getComponentType($taskName, self::TYPE_STATISTICS)
+		);
+
+		// Instantiate Manager
+        $managerProcess = self::getComponentType($taskName, self::TYPE_PROCESS);
+        $managerProcess->setExecutor($executor);
+                
+		$managerTrigger = self::getComponentType($taskName, self::TYPE_TRIGGER);
+        $managerTrigger->setQueue($queue);
+        
+        return $manager;
 	}
-	
+
+
+	public function get2($taskName) {
+		// Base Manager
+		$manager = self::getComponentType($taskName, self::TYPE_MANAGER);
+		
+        // Trigger, Queue & Statistics
+        $manager->setTrigger(
+            self::getComponentType($taskName, self::TYPE_TRIGGER)
+        );
+        $manager->getTrigger()->setQueue(
+            self::getComponentType($taskName, self::TYPE_QUEUE)
+        );
+        $manager->getTrigger()->getQueue()->setStatistics(
+            self::getComponentType($taskName, self::TYPE_STATISTICS)
+        );
+		
+		// Process, Executor & Status
+		$manager->setProcess(
+            self::getComponentType($taskName, self::TYPE_PROCESS)
+		);
+		$manager->getProcess()->setExecutor(
+            self::getComponentType($taskName, self::TYPE_EXECUTOR)
+		);
+        $manager->getProcess()->getExecutor()->setStatus(
+            self::getComponentType($taskName, self::TYPE_STATUS)
+        );
+		
+        return $manager;
+	}
+
+
 	/**
 	 * Returns an object of the specified objectType based on the taskName.  
 	 * @param string $taskName
 	 * @param string $objectType
 	 * @return stdClass
 	 */
-	public static function getObjectType($taskName, $objectType) {
+	public static function getComponentType($taskName, $objectType) {
 		// First: Check if the class has been overloaded
 		$class = $this->_getObjectClass($taskName, $objectType);
 		if (is_object($class)) {
@@ -108,50 +119,131 @@ class Factory {
 		$hardcoded = $this->_getObjectHardCoded($objectType);
 		return $hardcoded;
     }
-	
+
+
     /**
      * Returns the manager trigger for the specified task
      * @param string $taskName
      * @return \PhpTaskDaemon\Task\Manager\Trigger\AbstractClass
      */
 	public static function getManagerTrigger($taskName) {
-		return self::getObjectType($taskName, self::TYPE_MANAGER);
+		return self::getComponentType($taskName, self::TYPE_MANAGER);
 	}
-	
+
+
     /**
      * Returns the manager process for the specified task
      * @param string $taskName
      * @return \PhpTaskDaemon\Task\Manager\Process\AbstractClass
      */
 	public static function getManagerProcess($taskName) {
-		return self::getObjectType($taskName, self::TYPE_PROCESS);
+		return self::getComponentType($taskName, self::TYPE_PROCESS);
 	}
-	
+
+
     /**
      * Returns the executor status for the specified task
      * @param string $taskName
      * @return \PhpTaskDaemon\Task\Executor\Status\AbstractClass
      */
 	public static function getExecutorStatus($taskName) {
-		return self::getObjectType($taskName, self::TYPE_STATUS);
+		return self::getComponentType($taskName, self::TYPE_STATUS);
 	}
-	
+
+
     /**
      * Returns the queue for the specified task
      * @param string $taskName
      * @return \PhpTaskDaemon\Task\Queue\AbstractClass
      */
 	public static function getQueue($taskName) {
-		return self::getObjectType($taskName, self::TYPE_QUEUE);
+		return self::getComponentType($taskName, self::TYPE_QUEUE);
 	}
-	
+
+
     /**
      * Returns the queue statistics for the specified task
      * @param string $taskName
      * @return \PhpTaskDaemon\Task\Queue\Statistics\AbstractClass
      */
 	public static function getQueueStatistics($taskName) {
-        return self::getObjectType($taskName, self::TYPE_STATISTICS);
+        return self::getComponentType($taskName, self::TYPE_STATISTICS);
     }
-    
+
+
+    /**
+     * Returns the classname based on the taskName and objectType
+     * @param string $taskName
+     * @param string $objectType
+     * @return string
+     */
+    protected function _getClassName($taskName, $objectType) {
+        return '\\Tasks\\' . $taskName . '\\' . ucfirst($objectType);
+    }
+
+
+    /**
+     * Checks if a objectType class of a specific manager exists
+     * @param string $taskName
+     * @param string $objectType
+     * @return null|stdClass
+     */
+    protected function _getObjectClass($taskName, $objectType) {
+        $className = $this->_getClassName($taskName, $objectType);
+        if (class_exists($className)) {
+            return new $className();
+        }
+        return false;
+    }
+
+
+    /**
+     * Checks if task specific configuration options for the objectType are
+     * set.
+     * @param string $taskName
+     * @param string $objectType
+     * @return null|stdClass
+     */
+    protected function _getObjectTaskConfig($taskName, $objectType) {
+       return false;
+    }
+
+
+    /**
+     * Checks if default configuration options for the objectType are set.
+     * @param string $taskName
+     * @param string $objectType
+     * @return null|stdClass
+     */
+    protected function _getObjectDefaultConfig($taskName, $objectType) {
+    }
+
+
+    /**
+     * Returns the hardcoded default object for a specific type.
+     * @param string $objectType
+     * @return null|StdClass
+     */
+    protected function _getObjectHardCoded($objectType) {
+        switch($objectType) {
+            case 'manager':
+                return new \PhpTaskDaemon\Task\Manager\BaseClass();
+            case 'trigger':
+                return new \PhpTaskDaemon\Task\Manager\Trigger\BaseClass();
+            case 'queue':
+                return new \PhpTaskDaemon\Task\Queue\BaseClass();
+            case 'statistics':
+                return new \PhpTaskDaemon\Task\Queue\Statistics\BaseClass();
+            case 'process':
+                return new \PhpTaskDaemon\Task\Manager\Process\BaseClass();
+            case 'executor':
+                return new \PhpTaskDaemon\Task\Executor\BaseClass();
+            case 'status':
+                return new \PhpTaskDaemon\Task\Executor\Status\BaseClass();
+        }
+        throw new Exception\UndefinedObjectType('Unknown object type: ' . $objectType);
+        
+        return null;
+    }
+
 }
