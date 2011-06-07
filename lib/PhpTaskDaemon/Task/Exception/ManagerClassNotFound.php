37c51f9514d2cb2d1b79dbcac76efa68306626f1
Dirk Engels <d.engels@dirkengels.com>
2011-06-07 21:10:58 +0200

    moved exceptions and created new ones!

---
+++ lib/PhpTaskDaemon/Task/Exception/ManagerClassNotFound.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package PhpTaskDaemon
+ * @subpackage Task\Exception
+ * @copyright Copyright (C) 2011 Dirk Engels Websolutions. All rights reserved.
+ * @author Dirk Engels <d.engels@dirkengels.com>
+ * @license https://github.com/DirkEngels/PhpTaskDaemon/blob/master/doc/LICENSE
+ */
+namespace PhpTaskDaemon\Task\Exception;
+
+class ManagerObjectNotFound extends \Exception {
+
+}
