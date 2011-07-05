c6d88adbe7ab936c3a9f7d059d7531a597ed4394
Dirk Engels <d.engels@dirkengels.com>
2011-07-05 22:16:57 +0200

    added logger object

---
+++ lib/PhpTaskDaemon/Daemon/Logger.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @package PhpTaskDaemon
+ * @subpackage Daemon
+ * @copyright Copyright (C) 2011 Dirk Engels Websolutions. All rights reserved.
+ * @author Dirk Engels <d.engels@dirkengels.com>
+ * @license https://github.com/DirkEngels/PhpTaskDaemon/blob/master/doc/LICENSE
+ */
+
+namespace PhpTaskDaemon\Daemon;
+
+/**
+ * The logger component uses a Zend_Log object to log messages.
+ *
+ */
+class Logger {
+    protected static $_instance = null;
+    
+    /** 
+     * Protected constructor for singleton pattern
+     */
+    protected function __construct() {
+    }
+
+    /**
+     * Singleton getter
+     * @return \PhpTaskDaemon\Daemon\Log
+     */
+    public function get() {
+        if (!self::$_instance) {
+            self::$_instance = new \Zend_Log();
+            self::$_instance->addWriter(
+                new \Zend_Log_Writer_Null()
+            );
+            self::$_instance->log("Creating new log object", \Zend_Log::DEBUG);
+        }
+
+        return self::$_instance;
+    }
+}
