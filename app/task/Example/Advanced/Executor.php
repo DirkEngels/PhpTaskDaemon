ce3d759a9b38284023faab3cd7de00b544195862
Dirk Engels <d.engels@dirkengels.com>
2011-10-01 18:51:56 +0200

    change example task setting

--- app/task/Example/Advanced/Executor.php
+++ app/task/Example/Advanced/Executor.php
@@ -11,7 +11,7 @@ class Executor extends TaskExecutor\AbstractClass implements TaskExecutor\Interf
 
         // Sleep
         $sleepTimeProgress = round($job->getInput()->getVar('sleepTime')/10);
-        $sleepTimeProgress = 1000;
+        $sleepTimeProgress = rand(200, 1500) * 1000;
         for ($i=1; $i<10; $i++) {
             usleep($sleepTimeProgress);
             $this->updateStatus(($i*10), 'Task data: ' . $job->getJobId());
