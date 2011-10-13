27a6113df81f43959975107e26bc294222cac6de
Dirk Engels <d.engels@dirkengels.com>
2011-10-13 10:08:26 +0200

    added concept ipc filesystem implementation

---
+++ lib/PhpTaskDaemon/Daemon/Ipc/FileSystem.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * @package PhpTaskDaemon
+ * @subpackage Daemon\Ipc
+ * @copyright Copyright (C) 2011 Dirk Engels Websolutions. All rights reserved.
+ * @author Dirk Engels <d.engels@dirkengels.com>
+ * @license https://github.com/DirkEngels/PhpTaskDaemon/blob/master/doc/LICENSE
+ */
+
+namespace PhpTaskDaemon\Daemon\Ipc;
+
+/**
+ * 
+ * The Daemon\Ipc\FileSystem class is responsible for storing and retrieving
+ * inter process communication data from the database.
+ */
+class FileSystem extends AbstractClass implements InterfaceClass {
+
+    /**
+     * 
+     * Returns an empty array
+     * @return array
+     */
+    public function getKeys() {
+        return array();
+    }
+
+
+    /**
+     * 
+     * Returns nothing (NULL) 
+     * @param string $key
+     * @return mixed
+     */
+    public function getVar($key) {
+    }
+
+
+    /**
+     * 
+     * Sets nothing
+     * @param string $key
+     * @param mixed $value
+     * @return bool
+     */
+    public function setVar($key, $value) {
+        $sql = "REPLACE INTO ipc (key, value) VALUES ('" . $key . "','" . $value . "')";
+        return $this->_queryFileSystem($sql);
+    }
+
+
+    /**
+     * 
+     * Increments nothing
+     * @param string $key
+     * @return bool
+     */
+    public function incrementVar($key) {
+        $keyFile = $this->_getFileForKey($key);
+        $value = (int) file_get_contents($keyFile);
+        $value++;
+        return file_put_contents($keyFile, $value);
+    }
+
+
+    /**
+     * 
+     * Decrements the value of the key stored in a file.
+     * @param string $key
+     * @return bool
+     */
+    public function decrementVar($key) {
+        $keyFile = $this->_getFileForKey($key);
+        $value = (int) file_get_contents($keyFile);
+        if ($value > 0) {
+            $value--;
+            return file_put_contents($keyFile, $value);
+        }
+        return false;
+    }
+
+
+    /**
+     * 
+     * Removes the file from the filesystem 
+     * @param string $key
+     * @return bool
+     */
+    public function removeVar($key) {
+        $keyFile = $this->_getFileForKey($key);
+        if (file_exists($keyFile)) {
+            return unlink($keyFile);
+        }
+        return false;
+    }
+
+
+    /**
+     * 
+     * Removes nothing
+     * @return bool
+     */
+    public function remove() {
+    }
+
+    /**
+     * Returns the filename for a specific key
+     * @param string $key
+     * @output string
+     */
+    protected function _getFileForKey($key) {
+        return $this->getId() . '_' . strtolower($key);
+    }
+
+}
