918d0479f06ad34b71eb53b95677bbcd1472bd9d
Dirk Engels <d.engels@dirkengels.com>
2011-10-16 03:21:22 +0200

    code cleanup

--- tests/lib/PhpTaskDaemon/Task/Queue/DefaultClassTest.php
+++ tests/lib/PhpTaskDaemon/Task/Queue/DefaultClassTest.php
@@ -15,62 +15,66 @@
 namespace PhpTaskDaemon\Task\Queue;
 
 class DefaultClassTest extends \PHPUnit_Framework_Testcase {
-	protected $_queue;
-	protected $_statistics;
-	
-	protected function setUp() {
+    protected $_queue;
+    protected $_statistics;
+    
+    protected function setUp() {
         // Stop here and mark this test as incomplete.
         $this->markTestIncomplete(
           'This test has not been implemented yet.'
         );
 
-		$semaphore = __DIR__ . '/_data/constructor.shm';
-		$this->_queue = new \PhpTaskDaemon\Task\Queue\DefaultClass($semaphore);
-		$sharedMemory = new \PhpTaskDaemon\Daemon\Ipc\SharedMemory(\TMP_PATH . '/test-queue');
-		$this->_statistics = new \PhpTaskDaemon\Task\Queue\Statistics\DefaultClass($sharedMemory);
-	}
-	protected function tearDown() {
-//		$sharedMemory = $this->_statistics->getIpc();
-//		if (is_a($sharedMemory, '\PhpTaskDaemon\Daemon\Ipc\AbstractClass')) {
-//			$sharedMemory->remove();
-//		}
-//		unset($this->_statistics);
-	}
-	
-	public function testConstructor() {
-		$this->assertEquals(3, sizeof($this->_queue->load()));
-	}
-	public function testSetStatistics() {
-		$this->assertNull($this->_queue->getStatistics());
-		$this->_queue->setStatistics($this->_statistics);
-		$this->assertEquals($this->_statistics, $this->_queue->getStatistics());
-	}
-	public function testUpdateStatisticsNoStatisticsClassSet() {
-		$this->assertNull($this->_queue->getStatistics());
-		$this->assertFalse($this->_queue->updateStatistics('test'));
-	}
-	public function testUpdateStatisticsClassAlreadySet() {
-		$this->assertNull($this->_queue->getStatistics());
-		$this->_queue->setStatistics($this->_statistics);
-		$this->assertEquals($this->_statistics, $this->_queue->getStatistics());
-		$this->assertTrue($this->_queue->updateStatistics('test'));
-		$this->assertEquals(1, $this->_queue->getStatistics()->get('test'));
-		$this->assertTrue($this->_queue->updateStatistics('test', 10));
-		$this->assertEquals(10, $this->_queue->getStatistics()->get('test'));
-	}
+        $semaphore = __DIR__ . '/_data/constructor.shm';
+        $this->_queue = new \PhpTaskDaemon\Task\Queue\DefaultClass($semaphore);
+        $sharedMemory = new \PhpTaskDaemon\Daemon\Ipc\SharedMemory(\TMP_PATH . '/test-queue');
+        $this->_statistics = new \PhpTaskDaemon\Task\Queue\Statistics\DefaultClass($sharedMemory);
+    }
+    protected function tearDown() {
+//        $sharedMemory = $this->_statistics->getIpc();
+//        if (is_a($sharedMemory, '\PhpTaskDaemon\Daemon\Ipc\AbstractClass')) {
+//            $sharedMemory->remove();
+//        }
+//        unset($this->_statistics);
+    }
 
-	public function testUpdateQueueNoStatisticsClassSet() {
-		$this->assertNull($this->_queue->getStatistics());
-		$this->assertTrue($this->_queue->updateQueue(10));
-	}
-	public function testUpdateQueueStatisticsClassAlreadySet() {
-		$this->assertNull($this->_queue->getStatistics());
-		$this->_queue->setStatistics($this->_statistics);
-		$this->assertEquals($this->_statistics, $this->_queue->getStatistics());
-		$this->assertTrue($this->_queue->updateQueue(10));
-		$this->assertEquals(10, $this->_queue->getStatistics()->get('queued'));
-		$this->assertTrue($this->_queue->updateQueue());
-		$this->assertEquals(9, $this->_queue->getStatistics()->get('queued'));
-	}
-	
+    public function testConstructor() {
+        $this->assertEquals(3, sizeof($this->_queue->load()));
+    }
+
+    public function testSetStatistics() {
+        $this->assertNull($this->_queue->getStatistics());
+        $this->_queue->setStatistics($this->_statistics);
+        $this->assertEquals($this->_statistics, $this->_queue->getStatistics());
+    }
+
+    public function testUpdateStatisticsNoStatisticsClassSet() {
+        $this->assertNull($this->_queue->getStatistics());
+        $this->assertFalse($this->_queue->updateStatistics('test'));
+    }
+
+    public function testUpdateStatisticsClassAlreadySet() {
+        $this->assertNull($this->_queue->getStatistics());
+        $this->_queue->setStatistics($this->_statistics);
+        $this->assertEquals($this->_statistics, $this->_queue->getStatistics());
+        $this->assertTrue($this->_queue->updateStatistics('test'));
+        $this->assertEquals(1, $this->_queue->getStatistics()->get('test'));
+        $this->assertTrue($this->_queue->updateStatistics('test', 10));
+        $this->assertEquals(10, $this->_queue->getStatistics()->get('test'));
+    }
+
+    public function testUpdateQueueNoStatisticsClassSet() {
+        $this->assertNull($this->_queue->getStatistics());
+        $this->assertTrue($this->_queue->updateQueue(10));
+    }
+
+    public function testUpdateQueueStatisticsClassAlreadySet() {
+        $this->assertNull($this->_queue->getStatistics());
+        $this->_queue->setStatistics($this->_statistics);
+        $this->assertEquals($this->_statistics, $this->_queue->getStatistics());
+        $this->assertTrue($this->_queue->updateQueue(10));
+        $this->assertEquals(10, $this->_queue->getStatistics()->get('queued'));
+        $this->assertTrue($this->_queue->updateQueue());
+        $this->assertEquals(9, $this->_queue->getStatistics()->get('queued'));
+    }
+
 }
