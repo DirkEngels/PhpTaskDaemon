23eea8be0d6162e67682a405690334f6927e6bf0
Dirk Engels <d.engels@dirkengels.com>
2011-10-16 03:38:04 +0200

    add more unit tests

---
+++ tests/lib/PhpTaskDaemon/Daemon/ConsoleTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * @package PhpTaskDaemon
+ * @subpackage Daemon\Config
+ * @copyright Copyright (C) 2011 Dirk Engels Websolutions. All rights reserved.
+ * @author Dirk Engels <d.engels@dirkengels.com>
+ * @license https://github.com/DirkEngels/PhpTaskDaemon/blob/master/doc/LICENSE
+ *
+ * @group PhpTaskDaemon
+ * @group PhpTaskDaemon-Daemon
+ * @group PhpTaskDaemon-Daemon-Console
+ */
+
+
+namespace PhpTaskDaemon\Daemon\Pid;
+
+class ConsoleTest extends \PHPUnit_Framework_TestCase {
+    protected $_console;
+
+    protected function setUp() {
+        $this->_console = new \PhpTaskDaemon\Daemon\Console();
+    }
+
+    protected function tearDown() {
+    }
+
+    public function testConstructor() {
+        $this->assertInstanceOf('Zend_Console_Getopt', $this->_console->getConsoleOpts());
+        $this->assertInstanceOf('\\PhpTaskDaemon\\Daemon\\Instance', $this->_console->getInstance());
+    }
+
+    public function testConstructorWithInstance() {
+        $instance = new \PhpTaskDaemon\Daemon\Instance();
+        $this->_console = new \PhpTaskDaemon\Daemon\Console($instance);
+        $this->assertInstanceOf('\\PhpTaskDaemon\\Daemon\\Instance', $this->_console->getInstance());
+        $this->assertEquals($instance, $this->_console->getInstance());
+    }
+
+    /**
+     * @expectedExpeption \Exception
+     */
+    public function testSetConsoleOptsInvalid() {
+        $consoleOpts = new \Zend_Console_Getopt(array());
+        $this->_console->setConsoleOpts($consoleOpts);
+    }
+}
