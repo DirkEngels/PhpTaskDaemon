e8c4086dd87ceb078c73d8e6a3599c492c9bcfb6
Dirk Engels <d.engels@dirkengels.com>
2011-10-26 01:49:44 +0200

    refactored initializing the ipc adapter

--- lib/PhpTaskDaemon/Task/Queue/Statistics/AbstractClass.php
+++ lib/PhpTaskDaemon/Task/Queue/Statistics/AbstractClass.php
@@ -9,6 +9,8 @@
 
 namespace PhpTaskDaemon\Task\Queue\Statistics;
 
+use PhpTaskDaemon\Daemon\Config;
+
 /**
  * 
  * The abstract queue statistics class implements methods for setting/change
@@ -54,6 +56,20 @@ abstract class AbstractClass {
      * @return PhpTaskDaemon\Ipc
      */
     public function getIpc() {
+        if (is_null($this->_ipc)) {
+            $ipcClass = '\\PhpTaskDaemon\\Daemon\\Ipc\\' . Config::get()->getOptionValue('global.ipc');
+            if (!class_exists($ipcClass)) {
+                $ipcClass = '\\PhpTaskDaemon\\Daemon\\Ipc\\None';
+            }
+            $this->_ipc = new $ipcClass('phptaskdaemond-' . getmypid());
+
+            $this->_initializeStatus(self::STATUS_LOADED);
+            $this->_initializeStatus(self::STATUS_QUEUED);
+            $this->_initializeStatus(self::STATUS_RUNNING);
+            $this->_initializeStatus(self::STATUS_DONE);
+            $this->_initializeStatus(self::STATUS_FAILED);
+        }
+
         return $this->_ipc;
     }
 
@@ -65,11 +81,6 @@ abstract class AbstractClass {
      * @return $this
      */
     public function setIpc($ipc) {
-        if (!is_a($ipc, '\PhpTaskDaemon\Daemon\Ipc\AbstractClass')) {
-            $ipc = new \PhpTaskDaemon\Daemon\Ipc\None(
-                'statistics-' . getmypid()
-            );
-        }
         $this->_ipc = $ipc;
         $this->_initializeStatus(self::STATUS_LOADED);
         $this->_initializeStatus(self::STATUS_QUEUED);
@@ -150,9 +161,9 @@ abstract class AbstractClass {
      * @param string $status
      */
     private function _initializeStatus($status) {
-        $keys = $this->_ipc->getKeys();
+        $keys = $this->getIpc()->getKeys();
         if (!in_array($status, array_keys($keys))) {
-            $this->_ipc->setVar($status, 0);
+            $this->getIpc()->setVar($status, 0);
         }
     }
 
