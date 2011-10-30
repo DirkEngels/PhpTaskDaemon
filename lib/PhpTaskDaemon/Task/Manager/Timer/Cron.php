f886dd2262c53d3410f790e75b204e708c904e33
Dirk Engels <d.engels@dirkengels.com>
2011-10-30 04:42:12 +0100

    implemented cron timer using a cron expression library (https://github.com/mtdowling/cron-expression)

--- lib/PhpTaskDaemon/Task/Manager/Timer/Cron.php
+++ lib/PhpTaskDaemon/Task/Manager/Timer/Cron.php
@@ -9,6 +9,20 @@
 
 namespace PhpTaskDaemon\Task\Manager\Timer;
 
+require_once \LIBRARY_PATH . 'cron.phar';
+
 class Cron extends Interval {
+
+    public function getTimeToWait() {
+        $cron = Cron\CronExpression::factory('@daily');
+        $cron->isDue();
+        $nextRun = $cron->getNextRunDate();
+        $now = new DateTime();
+
+        if ($nextRun <= $now) {
+            return 0;
+        }
+        return $nextRun->getTimestamp() - $now->getTimestamp();
+    }
 
 }
