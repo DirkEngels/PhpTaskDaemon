d25ea719ec8f213e503f4394c9dbdbe7f372a074
Dirk Engels <dirk.engels@booking.com>
2012-03-26 18:40:47 +0200

    fixed coding style

--- lib/PhpTaskDaemon/Task/Executor/ExecutorInterface.php
+++ lib/PhpTaskDaemon/Task/Executor/ExecutorInterface.php
@@ -11,13 +11,13 @@ namespace PhpTaskDaemon\Task\Executor;
 interface ExecutorInterface {
 
     public function getJob();
-    public function setJob(\PhpTaskDaemon\Task\Job\JobAbstract $job);
+    public function setJob( \PhpTaskDaemon\Task\Job\JobAbstract $job );
     public function getIpc();
-    public function setIpc(\PhpTaskDaemon\Daemon\Ipc\IpcAbstract $ipc);
+    public function setIpc( \PhpTaskDaemon\Daemon\Ipc\IpcAbstract $ipc );
     public function resetIpc();
     public function resetPid();
-    public function getStatus($key);
-    public function setStatus($percentage, $message);
+    public function getStatus( $key );
+    public function setStatus( $percentage, $message );
     public function run();
 
 }
