675aeb9a87a77182cabce651769e4def191b10c5
Dirk Engels <dirk.engels@booking.com>
2012-05-19 23:22:32 +0200

    remove ipc in setup and teardown

--- tests/lib/PhpTaskDaemon/Task/Executor/ExecutorDefaultTest.php
+++ tests/lib/PhpTaskDaemon/Task/Executor/ExecutorDefaultTest.php
@@ -17,30 +17,20 @@ class ExecutorDefaultTest extends \PHPUnit_Framework_TestCase {
     protected $_executor;
     protected $_status;
     protected $_job;
-    
-    protected function setUp() {
-//        // Stop here and mark this test as incomplete.
-//        $this->markTestIncomplete(
-//          'This test has not been implemented yet.'
-//        );
 
+    protected function setUp() {
         $this->_executor = new \PhpTaskDaemon\Task\Executor\ExecutorDefault();
         $this->_job = new \PhpTaskDaemon\Task\Job\JobDefault();
-
-        // Mark empty run function of the base class as executed in the coverage
-        $this->_executor->run();
     }
 
     protected function tearDown() {
-//        $ipc = $this->_status->getIpc();
-//        if (is_a($ipc, '\PhpTaskDaemon\Daemon\Ipc\IpcAbstract')) {
-//            $ipc->remove();
-//        }
-//        unset($this->_status);
     }
 
 
     public function testSetJob() {
+        // Mark empty run function of the base class as executed in the coverage
+        $this->_executor->run();
+
         $this->assertNull($this->_executor->getJob());
         $this->_executor->setJob($this->_job);
         $this->assertEquals($this->_job, $this->_executor->getJob());
